9fe9e904039923c8e7ba6379b9f447ba36f42f1c
= <=>
2025-06-09 23:05:39 -0300

    atualizando o next.config e gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  distDir: "dist",
+  images: { unoptimized: true },
+  //   reactStrictMode: true,
+  // basePath:
+  //     process.env.NODE_ENV === "production" ? "/KevenFernandes.github.io" : "",
+  //   assetPrefix:
+  //     process.env.NODE_ENV === "production" ? "/KevenFernandes.github.io/" : "",
 };
 
 export default nextConfig;
